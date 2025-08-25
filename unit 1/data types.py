d898d9779169cdb4e88fa0b6ef055dc1af6b960c
lgalley659 <lgalley659@boyslatin.org>
2025-08-25 13:14:34 +0000

    classwork

--- unit 1/data types.py
+++ unit 1/data types.py
@@ -1,4 +1,22 @@
 #Strings-Anything wrapped in quotes
 "Lumen Galley
 "Good morning. have a good day.
-"123#$%"
+"1345"
+#comments- invisible text that only
+#humans can read
+# comments are created with the hashtag symbol
+ 
+ # Integers- whole numbers
+ 10
+ 120
+ 34000
+
+ # Floats - decimal numbers
+ 0.1 
+ 20.03
+
+ # Boolean - values that represent true and false
+ True
+ False 
+
+# Variable - a container
