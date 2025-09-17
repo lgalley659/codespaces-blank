2d02862e8a78dcee461bcbbdfb7923907870e7cd
lgalley659 <lgalley659@boyslatin.org>
2025-09-17 13:37:33 +0000

    classwork

--- unit 2/functions.py
+++ unit 2/functions.py
@@ -22,6 +22,30 @@ example2()
 def math():
     a = input ('enter a number: ')
     b = 10    
-    print(a + b)
+    print(int(a) + b)
 
 math()
+
+# phase 1: function definition
+# we are describing the instruction for our custom code.
+# we are adding logic to the comuters vocabulary.
+# this code does not run- it only gives it meaning
+# not the action.
+ 
+# we indent to inform the computer that we are about to write 
+# code instructions. If we dont, we will get an error.
+
+# Create a function that calculates 2 unique user inputed
+
+def calculate():
+    a = input('Please enter a number: ')
+    b = input('Please ender another number: ')
+    print(int(a) + int(b))
+    print(int(a) - int(b))
+    print(int(a) / int(b))
+    print(int(a) * int(b))
+    print(int(a) % int(b))
+    print(int(a) ** int(b))
+    print(int(a) // int(b))
+
+calculate()
