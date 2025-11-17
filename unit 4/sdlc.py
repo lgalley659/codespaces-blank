27211a155d2c691196ca3d34412490e3f1af16fb
lgalley659 <lgalley659@boyslatin.org>
2025-11-17 14:17:21 +0000

    sdlc

--- unit 4/sdlc.py
+++ unit 4/sdlc.py
@@ -17,6 +17,8 @@ def signup():
         tiktok_standard.append(userAge)
 signup()
 
+
+
 # Step 1. ideation - what features should you have in your calculator app?
 # Please write the 3 or 4 features as strings.
 feature1 = "Addition"
@@ -29,3 +31,25 @@ print("Welcome to the Calculator App!")
 # Please write atleast 1 function for 1 of your features.
 def addition(num1, num2):
     return num1 + num2
+def subtraction(num1, num2):
+    return num1 - num2
+def multiplication(num1, num2):
+    return num1 * num2
+def division(num1, num2):
+    if num2 != 0:
+        return num1 / num2
+    else:
+        return "Error! Division by zero."
+
+def add():
+    num1 = int(input("Please type in a number: "))
+    num2 = int(input("Please type in another number: "))
+    sum = num1 + num2
+    print(sum)
+    bonusNum = int(input("would you like to add another number to the sum?:" ))
+    if bonusNum: == 'y':
+             numX = int(input("Please type in number: "))
+             sum += numX
+             print(sum)
+    
+        
